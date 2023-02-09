130c0f9623d97c224ed825d0a29ade709768ccf5
Acer <Acer@172.16.68.132>
2023-02-09 10:18:16 +0700

    Add all file

---
+++ src/myhouse/MyHouse.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myhouse;
+
+/**
+ *
+ * @author Acer
+ */
+public class MyHouse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
